c2361f8d606a196f89e7ec3900b1ef18b0cfbf59
Joseph Powell <japowell@usc.edu>
2019-02-04 22:21:47 -0800

    serial

--- OpenCV_code/webcam.py
+++ OpenCV_code/webcam.py
@@ -1,9 +1,21 @@
 import cv2
 import sys
-
+from serial import Serial
+import threading
 import time
+from struct import pack, unpack
 
 currentx = 0
+
+def connect():
+    try:
+        conn = Serial('/dev/tty.usbmodem14201', baudrate=9600, dsrdtr=0, rtscts=0, timeout=1) #cu.usbmodemFA131
+    except IOError:
+        print("Error opening serial port.")
+        sys.exit(2)
+        
+    return conn
+
 def largestArea(faces):
     largestx = 0
     largesty = 0
@@ -21,6 +33,22 @@ def largestArea(faces):
 
     cv2.rectangle(frame, (largestx, largesty), (largestx+largestw, largesty+largesth), (0, 255, 0), 2)
     return largestx
+
+def read(port):
+    print("reading from port")
+    while True:
+        
+        line = port.read(32)
+        #temp = unpack('=i', line)
+        print(line)
+
+print("Connecting...")
+conn = connect()
+print("Connected!")
+        
+print("Running code...")
+t1 = threading.Thread(target=read, args=(conn,))
+t1.start()
 
 cascPath = "haarcascade_frontalface_default.xml"
 #cascPath = "haarcascade_upperbody.xml"
@@ -50,9 +78,12 @@ while True:
     change = newx - currentx
     cv2.putText(frame, "Delta : " + str(int(change)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2)
 
+    #print(change)
+    packed_data = pack('=i', change)
+    #print(packed_data)
+    conn.write(packed_data)
+    
     currentx = newx
-
-
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
@@ -60,7 +91,7 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    time.sleep(0.1)
+    time.sleep(0.25)
 
 # When everything is done, release the capture
 video_capture.release()
